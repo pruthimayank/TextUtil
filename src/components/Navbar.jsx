366f610d376e986cfdea48910ed3994088c499e1
Mayank <pruthimayank2003@gmail.com>
2023-03-24 02:53:51 +0530

    Initial commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,7 +5,7 @@ export default function Navbar(props) {
     <div>
       <nav className={`navbar navbar-${props.mode} bg-${props.mode} border-bottom border-dark-subtle rounded-bottom-4`}>
         <div className="container-fluid">
-          <a className="navbar-brand" href="/">TextUtils</a>
+          <a className="navbar-brand" href="/">TextUtil</a>
           <div>
             <div className="form-check form-check-inline">
               <input className="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio1" value="option1" style={{ backgroundColor: "white" }} onClick={props.tLT} />
